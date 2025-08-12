25b77bbe876234d0cd95d87962847255d7f258ce
Ruth-Hdz <138893415+Ruth-Hdz@users.noreply.github.com>
2025-08-11 23:17:23 -0600

    Ultima actualizacion

--- src/database.js
+++ src/database.js
@@ -1,14 +1,22 @@
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
+import fs from 'fs';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    ca: fs.readFileSync(process.env.DB_SSL_CA_PATH)
+  },
+  waitForConnections: true,
   connectionLimit: 10,
+  queueLimit: 0,
+  charset: 'utf8mb4',
 });
 
-export default pool.promise();
+export default pool;
